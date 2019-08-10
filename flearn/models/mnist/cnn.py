7435eede315e2322f1177dbaee38bee79f36cd89
CharlieDinh <thecanhdinh92@gmail.com>
2019-08-11 00:07:13 +1000

    update cnn

--- flearn/models/mnist/cnn.py
+++ flearn/models/mnist/cnn.py
@@ -121,32 +121,33 @@ class Model(object):
                     with self.graph.as_default():
                         self.sess.run(self.train_op, feed_dict={
                                       self.features: X, self.labels: y})
-        if(optimizer == "fedprox"):
+
+        if(optimizer == "fedprox" or optimizer == "fedsgd"):
             data_x, data_y = suffer_data(data)
             for _ in range(num_epochs):  # t = 1,2,3,4,5,...m
                 X, y = get_random_batch_sample(data_x, data_y, batch_size)
                 with self.graph.as_default():
                     self.sess.run(self.train_op, feed_dict={
                         self.features: X, self.labels: y})
-        else:
-            wzero = self.get_params()
+
+        if(optimizer == "fedsarah" or optimizer == "fedsvrg"):
             data_x, data_y = suffer_data(data)
+
+            wzero = self.get_params()
             w1 = wzero - self.optimizer._lr * np.array(self.vzero)
-            w1 = prox_L2(np.array(w1), np.array(wzero),
-                         self.optimizer._lr, self.optimizer._lamb)
+            w1 = prox_L2(np.array(w1), np.array(wzero),self.optimizer._lr, self.optimizer._lamb)
             self.set_params(w1)
 
-            for _ in range(num_epochs):  # t = 1,2,3,4,5,...m
+            for e in range(num_epochs-1):  # t = 1,2,3,4,5,...m
                 X, y = get_random_batch_sample(data_x, data_y, batch_size)
                 with self.graph.as_default():
                     # get the current weight
                     if(optimizer == "fedsvrg"):
                         current_weight = self.get_params()
-
+                    
                         # calculate fw0 first:
                         self.set_params(wzero)
-                        fwzero = self.sess.run(self.grads, feed_dict={
-                                               self.features: X, self.labels: y})
+                        fwzero = self.sess.run(self.grads, feed_dict={self.features: X, self.labels: y})
                         self.optimizer.set_fwzero(fwzero, self)
 
                         # return the current weight to the model
@@ -154,32 +155,29 @@ class Model(object):
                         self.sess.run(self.train_op, feed_dict={
                             self.features: X, self.labels: y})
                     elif(optimizer == "fedsarah"):
-                        if(_ == 0):
+                        if(e == 0):
                             self.set_params(wzero)
                             grad_w0 = self.sess.run(self.grads, feed_dict={
                                                     self.features: X, self.labels: y})  # grad w0)
                             self.optimizer.set_preG(grad_w0, self)
+
                             self.set_params(w1)
                             preW = self.get_params()   # previous is w1
+
                             self.sess.run(self.train_op, feed_dict={
                                 self.features: X, self.labels: y})
-                        else:
-                         # == w1
+                        else: # == w1
                             curW = self.get_params()
 
                             # get previous grad
                             self.set_params(preW)
-                            grad_preW = self.sess.run(self.grads, feed_dict={
-                                                      self.features: X, self.labels: y})  # grad w0)
+                            grad_preW = self.sess.run(self.grads, feed_dict={self.features: X, self.labels: y})  # grad w0)
                             self.optimizer.set_preG(grad_preW, self)
                             preW = curW
-                            # return back curent grad
+
+                            # return back curent grad 
                             self.set_params(curW)
-                            self.sess.run(self.train_op, feed_dict={
-                                          self.features: X, self.labels: y})
-                    else:   # fedsgd
-                        self.sess.run(self.train_op, feed_dict={
-                                      self.features: X, self.labels: y})
+                            self.sess.run(self.train_op, feed_dict={self.features: X, self.labels: y})
         soln = self.get_params()
         comp = num_epochs * \
             (len(data['y'])//batch_size) * batch_size * self.flops
