861b7b2b229d861980c21c04f82c6c23ebf74d2b
CharlieDinh <thecanhdinh92@gmail.com>
2019-12-30 16:38:59 +1100

    update plot

--- flearn/utils/plot_utils.py
+++ flearn/utils/plot_utils.py
@@ -29,6 +29,7 @@ def plot_summary_one_figure(num_users=100, loc_ep1=5, Numb_Glob_Iters=10, lamb=[
     
     plt.figure(1)
     MIN = train_loss.min() - 0.001
+    start = 200
     linestyles = ['-', '--', '-.', ':', '-', '--', '-.', ':']
     for i in range(Numb_Algs):
         plt.plot(train_acc[i, 1:], linestyle=linestyles[i], label=algs_lbl[i] + str(lamb[i])+ "_"+str(loc_ep1[i])+"e" + "_" + str(batch_size[i]) + "b")
@@ -42,7 +43,7 @@ def plot_summary_one_figure(num_users=100, loc_ep1=5, Numb_Glob_Iters=10, lamb=[
 
     plt.figure(2)
     for i in range(Numb_Algs):
-        plt.plot(train_loss[i, 1:], linestyle=linestyles[i], label=algs_lbl[i] + str(lamb[i])+
+        plt.plot(train_loss[i, start:], linestyle=linestyles[i], label=algs_lbl[i] + str(lamb[i])+
                  "_"+str(loc_ep1[i])+"e" + "_" + str(batch_size[i]) + "b")
         #plt.plot(train_loss1[i, 1:], label=algs_lbl1[i])
     plt.legend(loc='upper right')
@@ -56,7 +57,7 @@ def plot_summary_one_figure(num_users=100, loc_ep1=5, Numb_Glob_Iters=10, lamb=[
 
     plt.figure(3)
     for i in range(Numb_Algs):
-        plt.plot(glob_acc[i, 1:], linestyle=linestyles[i],
+        plt.plot(glob_acc[i, start:], linestyle=linestyles[i],
                  label=algs_lbl[i]+str(lamb[i])+"_"+str(loc_ep1[i])+"e" + "_" + str(batch_size[i]) + "b")
         #plt.plot(glob_acc1[i, 1:], label=algs_lbl1[i])
     plt.legend(loc='lower right')
@@ -393,3 +394,88 @@ def plot_summary_three_figures_batch(num_users=100, loc_ep1=[], Numb_Glob_Iters=
     plt.savefig(dataset + str(loc_ep1[1]) +
                 'testing_accuracy.png', bbox_inches='tight')
 
+
+def plot_summary(num_users=100, loc_ep1=[], Numb_Glob_Iters=10, lamb=[], learning_rate=[], algorithms_list=[],batch_size=0, dataset=""):
+    
+    #+'$\mu$'\
+    Numb_Algs = len(algorithms_list)
+    train_acc = np.zeros((Numb_Algs, Numb_Glob_Iters))
+    train_loss = np.zeros((Numb_Algs, Numb_Glob_Iters))
+    glob_acc = np.zeros((Numb_Algs, Numb_Glob_Iters))
+    algs_lbl = algorithms_list.copy()
+    for i in range(Numb_Algs):
+        if(lamb[i] > 0):
+            algorithms_list[i] = algorithms_list[i] + "_prox_" + str(lamb[i])
+            algs_lbl[i] = algs_lbl[i] + "_prox"
+        algorithms_list[i] = algorithms_list[i] + \
+            "_" + str(learning_rate[i]) + "_" + str(num_users) + \
+            "u" + "_" + str(batch_size[i]) + "b"
+        train_acc[i, :], train_loss[i, :], glob_acc[i, :] = np.array(
+            simple_read_data(loc_ep1[i], dataset + algorithms_list[i]))[:, :Numb_Glob_Iters]
+        algs_lbl[i] = algs_lbl[i]
+
+    plt.figure(1)
+    linestyles = ['-', '--', '-.', '-', '--', '-.']
+    algs_lbl = ["FedProxVR_Sarah", "FedProxVR_Svrg", "FedAvg",
+                "FedProxVR_Sarah", "FedProxVR_Svrg", "FedAvg"]
+    fig = plt.figure(figsize=(10, 4))
+    ax = fig.add_subplot(111)    # The big subplot
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    max = 0.6
+    min = train_loss.min() - 0.001
+    num_al = 3
+    interation_start = 200
+# Turn off axis lines and ticks of the big subplot
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['right'].set_color('none')
+    ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+    
+    for i in range(num_al):
+        ax2.plot(train_loss[i, interation_start:], linestyle=linestyles[i], label=algs_lbl[i] + " : " + '$\mu = $' + str(lamb[i]))
+        ax2.set_ylim([min, max])
+        ax2.legend()
+        ax2.set_title("MNIST: " + r'$\beta = 7,$' + r'$\tau = 20$', y=1.02)
+    
+    for i in range(num_al):
+        ax1.plot(train_loss[i+num_al, interation_start:], linestyle=linestyles[i], label=algs_lbl[i + num_al] + " : " + '$\mu = $' + str(lamb[i]))
+        ax1.set_ylim([min, max])
+        ax1.legend()
+        ax1.set_title("MNIST: " + r'$\beta = 5,$' + r'$\tau = 10$', y=1.02)
+            
+    ax.set_xlabel('Number of Global Iterations')
+    ax.set_ylabel('Training Loss', labelpad=15)
+    plt.savefig('train_loss.pdf')
+    plt.savefig(dataset.upper() + str(loc_ep1[1]) + 'train_loss.png')
+
+    plt.figure(2)
+    fig = plt.figure(figsize=(10, 4))
+    ax = fig.add_subplot(111)    # The big subplot
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    max = glob_acc.max() + 0.01
+    # Turn off axis lines and ticks of the big subplot
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['right'].set_color('none')
+    ax.tick_params(labelcolor='w', top='off',
+                   bottom='off', left='off', right='off')
+    
+    for i in range(3):
+        ax2.plot(glob_acc[i, 1:], linestyle=linestyles[i], label=algs_lbl[i] + " : " + '$\mu = $' + str(lamb[i]))
+        ax2.set_ylim([0.8, max])
+        ax2.legend()
+        ax2.set_title("MNIST: " + r'$\beta = 7,$' + r'$\tau = 20$', y=1.02)
+
+    for (i) in range(3):
+        ax1.plot(glob_acc[i+3, 1:], linestyle=linestyles[i+3],label=algs_lbl[i] + " : " + '$\mu = $' + str(lamb[i]))
+        ax1.set_title("MNIST: " + r'$\beta = 5,$' + r'$\tau = 10$', y=1.02)
+        ax1.set_ylim([0.8, max])
+        ax1.legend()
+    ax.set_xlabel('Number of Global Iterations')
+    ax.set_ylabel('Test Accuracy', labelpad=15)
+    plt.savefig('glob_acc.pdf')
+    plt.savefig(dataset.upper() + str(loc_ep1[1]) + 'glob_acc.png')
