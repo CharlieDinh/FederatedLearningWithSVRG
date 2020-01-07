3788699be8f01548bae02524346034a803820b84
CharlieDinh <thecanhdinh92@gmail.com>
2020-01-07 11:25:02 +1100

    Update generate and plot for synthetic data

--- flearn/utils/plot_utils.py
+++ flearn/utils/plot_utils.py
@@ -672,7 +672,6 @@ def plot_summary_one_figure2(num_users=100, loc_ep1=5, Numb_Glob_Iters=10, lamb=
     plt.savefig(dataset.upper() + str(loc_ep1[1]) + 'glob_acc.png')
     #plt.savefig(dataset + str(loc_ep1[1]) + 'glob_acc.pdf')
 
-
 def get_max_value_index(num_users=100, loc_ep1=5, Numb_Glob_Iters=10, lamb=[], learning_rate=[], algorithms_list=[], batch_size=0, dataset=""):
     Numb_Algs = len(algorithms_list)
     glob_acc, train_acc, train_loss = get_training_data_value(
@@ -680,3 +679,97 @@ def get_max_value_index(num_users=100, loc_ep1=5, Numb_Glob_Iters=10, lamb=[], l
     for i in range(Numb_Algs):
         print("Algorithm: ", algorithms_list[i], "Max testing Accurancy: ", glob_acc[i].max(
         ), "Index: ", np.argmax(glob_acc[i]), "local update:", loc_ep1[i])
+
+def plot_summary_synthetic(num_users=100, loc_ep1=[], Numb_Glob_Iters=10, lamb=[], learning_rate=[], algorithms_list=[]):       
+    #+'$\mu$'
+    Numb_Algs = len(algorithms_list)
+    train_acc = np.zeros((Numb_Algs, Numb_Glob_Iters))
+    train_loss = np.zeros((Numb_Algs, Numb_Glob_Iters))
+    glob_acc = np.zeros((Numb_Algs, Numb_Glob_Iters))
+    algs_lbl = algorithms_list.copy()
+    for i in range(Numb_Algs):
+        if(lamb[i] > 0):
+            algorithms_list[i] = algorithms_list[i] + "_prox_" + str(lamb[i])
+            algs_lbl[i] = algs_lbl[i] + "_prox"
+        algorithms_list[i] = algorithms_list[i] + "_" + str(learning_rate[i]) + "_" + str(num_users) + "u"
+        train_acc[i, :], train_loss[i, :], glob_acc[i, :] = np.array(
+            simple_read_data(loc_ep1[i], DATA_SET + algorithms_list[i]))[:, :Numb_Glob_Iters]
+        algs_lbl[i] = algs_lbl[i]
+
+    plt.figure(1)
+    linestyles = ['-', '--', '-.', '-', '--', '-.']
+    #color = ['b', 'r', 'g', 'y']
+    #plt.subplot(121)
+    #for i in range(Numb_Algs):
+    #    plt.plot(train_acc[i, 1:], linestyle=linestyles[i],
+    #plt.plot(train_acc1[i, 1:], label=algs_lbl1[i])
+    #lt.legend(loc='best')
+    #plt.ylabel('Training Accuracy')
+    #plt.xlabel('Number of Global Iterations')
+    #plt.title(DATA_SET)
+    #plt.savefig('train_acc.png')
+    #fig, axes = plt.subplots(1, 2, sharex=True, sharey=True)
+    
+    fig = plt.figure(figsize=(10, 4))
+    ax = fig.add_subplot(111)    # The big subplot
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    min = train_loss.min() - 0.01
+    #min = 0.14
+    algs_lbl = ["FedProxVR_Svrg", "FedProxVR_Svrg", "FedProxVR_Svrg", "FedProxVR_Svrg",
+                "FedProxVR_Sarah", "FedProxVR_Sarah", "FedProxVR_Sarah", "FedProxVR_Sarah"]
+# Turn off axis lines and ticks of the big subplot
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['right'].set_color('none')
+    ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+    num = 4
+    for i in range(num):
+        ax2.plot(train_loss[i, 1:], linestyle=linestyles[i], label=algs_lbl[i] + " : " + '$\mu = $' + str(lamb[i]))
+        ax2.set_ylim([min, 1.2])
+        ax2.legend()
+        #ax2.set_title("Synthetic_0_0 " + r'$\beta = 15,$' + r'$\tau = 20$', y=1.02)
+    
+    for (i) in range(num):
+        ax1.plot(train_loss[i+num, 1:], linestyle=linestyles[i],
+                 label=algs_lbl[i+num] + " : " + '$\mu = $' + str(lamb[i]))
+        ax1.set_ylim([min, 1.2])
+        ax1.legend()
+        #ax1.set_title("Synthetic_0_0 : " + r'$\beta = 15,$' + r'$\tau = 20$', y=1.02)
+    ax.set_title("Synthetic: 100 users, " + r'$\beta = 7,$' +
+                  r'$\tau = 20$', y=1.02)
+    ax.set_xlabel('Number of Global Iterations')
+    ax.set_ylabel('Training Loss', labelpad=15)
+    plt.savefig('train_loss.pdf')
+
+    plt.figure(2)
+    fig = plt.figure(figsize=(10, 4))
+    ax = fig.add_subplot(111)    # The big subplot
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    max = glob_acc.max() + 0.01
+    # Turn off axis lines and ticks of the big subplot
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['right'].set_color('none')
+    ax.tick_params(labelcolor='w', top='off',
+                   bottom='off', left='off', right='off')
+    for i in range(num):
+        ax2.plot(glob_acc[i, 1:], linestyle=linestyles[i],
+                 label=algs_lbl[i] + " : " + '$\mu = $' + str(lamb[i]))
+        #ax2.set_ylim([0.8, max])
+        ax1.set_ylim([0.5, max])
+        ax2.legend()
+        ax2.set_title("FASHION MNIST: " + r'$\beta = 7,$' +
+                      r'$\tau = 50$', y=1.02)
+    for (i) in range(num):
+        ax1.plot(glob_acc[i+num, 1:], linestyle=linestyles[i],
+                 label=algs_lbl[i+num] + " : " + '$\mu = $' + str(lamb[i]))
+        ax1.set_title("FASHION MNIST: " + r'$\beta = 15,$' + r'$\tau = 20$', y=1.02)
+        ax1.set_ylim([0.5, max])
+        ax1.legend()
+    ax.set_xlabel('Number of Global Iterations')
+    ax.set_ylabel('Test Accuracy', labelpad=15)
+    plt.savefig('glob_acc.png')
